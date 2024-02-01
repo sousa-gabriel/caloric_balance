2e96c042a1ecda82a5ba33ad08d11413e718eb8c
gabriel.rocha <gabriel.rocha@paripassu.com.br>
2024-02-01 16:08:36 -0300

    creating component Input from app

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import * as S from './InputStyle'
+import { Roboto } from '../roboto/Roboto'
+import { theme } from '../../theme/theme'
+
+export interface IInput {
+  value: string
+  label: string
+  placeholder: string
+  errorMessage?: string
+  onChangeText: (value: string) => void
+  resetError?: () => void
+}
+
+export function Input({
+  label,
+  placeholder,
+  errorMessage,
+  value,
+  onChangeText,
+  resetError,
+}: IInput) {
+  const [isFocused, setIsFocused] = useState(false)
+  const colorFocused = isFocused ? 'primary' : 'secondary'
+  const colorInput = !!errorMessage ? 'error' : colorFocused
+
+  function handleInputFocus() {
+    setIsFocused(true)
+    resetError && resetError()
+  }
+
+  function handleInputBlur() {
+    setIsFocused(false)
+  }
+
+  return (
+    <S.InputContainer>
+      <Roboto text={label} color={colorInput} textStyles="LargeSemiBold" />
+      <S.Input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        color={colorInput}
+        placeholder={placeholder}
+        value={value}
+        onChangeText={onChangeText}
+        placeholderTextColor={theme.colors.gray400}
+      />
+      {errorMessage && (
+        <Roboto color="error" text={errorMessage} textStyles="MediumRegular" />
+      )}
+    </S.InputContainer>
+  )
+}
---
+++ src/components/input/InputStyle.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native'
+import { normalize } from '../../utils/normalize'
+
+interface IInputStyle {
+  color: 'primary' | 'secondary' | 'error'
+}
+
+export const InputContainer = styled.View`
+  width: 100%;
+`
+export const Input = styled.TextInput<IInputStyle>`
+  height: ${normalize(60)}px;
+  border-width: 1px;
+  border-color: ${({ theme, color }) => theme.colors[color]};
+  margin: ${normalize(16)}px 0px;
+  border-radius: 5px;
+  width: 100%;
+  padding: 0px ${normalize(16)}px;
+  color: ${({ theme }) => theme.colors.secondary};
+`
