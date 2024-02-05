8f6b94e7e98763611c7b700e345a934d6e139f0d
gabriel.rocha <gabriel.rocha@paripassu.com.br>
2024-02-05 13:59:29 -0300

    Finish screen login

--- src/screens/auth/loginScreen/Login.tsx
+++ src/screens/auth/loginScreen/Login.tsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from './LoginStyle'
-import { Calendar } from '../../../components/calendar/Calendar'
-import { useCalendar } from '../../../components/calendar/useCalendar'
+import { Button, ButtonRow, Input, Screen } from '@components'
+import LogoDark from '../../../assets/imagens/png/logoDark.png'
+import LogoLight from '../../../assets/imagens/png/logoLight.png'
+import { isDarkMode } from '@theme'
+import { SocialLogin } from './components/SocialLogin/SocialLogin'
 
 export function Login() {
-  const { markedDates, onDayPress, interval } = useCalendar()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <S.ContainerLogin>
-      <Calendar
-        markedDates={markedDates}
-        onDayPress={onDayPress}
-        StartMarkedDates={interval}
+    <Screen>
+      <S.ContainerImageLogo>
+        <S.ImageLogo source={isDarkMode ? LogoDark : LogoLight} />
+      </S.ContainerImageLogo>
+      <Input
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+        onChangeText={setEmail}
+        value={email}
       />
-    </S.ContainerLogin>
+      <Input
+        label="Senha"
+        placeholder="Digite sua senha"
+        onChangeText={setPassword}
+        value={password}
+        isInputPassword
+      />
+      <Button
+        onPress={() => {}}
+        title="Esqueci minha senha"
+        typeButton="ButtonText"
+        style={{ justifyContent: 'flex-start' }}
+      />
+      <ButtonRow
+        buttonPrimaryOnPress={() => {}}
+        buttonSecondaryOnPress={() => {}}
+        buttonPrimaryTitle="Entrar"
+        buttonSecondaryTitle="Criar conta"
+      />
+      <SocialLogin />
+    </Screen>
   )
 }
--- src/screens/auth/loginScreen/LoginStyle.ts
+++ src/screens/auth/loginScreen/LoginStyle.ts
@@ -1,9 +1,41 @@
 import styled from 'styled-components/native'
 
-export const ContainerLogin = styled.View`
+export const ImageLogo = styled.Image``
+
+export const ContainerImageLogo = styled.View`
   width: 100%;
-  flex: 1;
-  justify-content: center;
   align-items: center;
-  background: ${({ theme }) => theme.colors.background};
+  justify-content: center;
+  margin-bottom: ${({ theme }) => theme.spaces.sp40}px;
+`
+export const ContainerButton = styled.View`
+  flex-direction: row;
+  width: 100%;
+`
+export const ContentButton = styled.View`
+  flex: 1;
+`
+
+export const divider = styled.View`
+  width: ${({ theme }) => theme.spaces.sp16}px;
+`
+export const ContainerButtonLoginSocial = styled.View`
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  justify-content: space-around;
+  border-top-width: 1px;
+  border-top-color: ${({ theme }) => theme.colors.gray300};
+  margin-top: ${({ theme }) => theme.spaces.sp40}px;
+  padding-top: ${({ theme }) => theme.spaces.sp40}px;
+`
+export const IconCircle = styled.TouchableOpacity`
+  width: ${({ theme }) => theme.spaces.sp40}px;
+  height: ${({ theme }) => theme.spaces.sp40}px;
+  border-radius: ${({ theme }) => theme.spaces.sp32}px;
+  border-color: ${({ theme }) => theme.colors.gray400};
+  border-width: 0.5px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.gray100};
 `
