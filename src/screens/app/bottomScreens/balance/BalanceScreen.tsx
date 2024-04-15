a6a8f31bc4751f5f23484988e20a7d38ef855a17
gabriel.rocha <gabriel.rocha@paripassu.com.br>
2024-04-14 22:30:06 -0300

    start IA for Recipe and IA for photo Balance

--- src/screens/app/bottomScreens/balance/BalanceScreen.tsx
+++ src/screens/app/bottomScreens/balance/BalanceScreen.tsx
@@ -1,10 +1,72 @@
-import React from 'react'
-import { HeaderScreens, Screen } from '@components'
+import React, { useState } from 'react'
+import { ButtonRow, HeaderScreens, Screen } from '@components'
+import { launchImageLibrary, launchCamera } from 'react-native-image-picker'
+import { Image } from 'react-native'
 
 export function BalanceScreen() {
+  const [photo, setPhoto] = useState<string | null>(null)
+
+  async function openCamera() {
+    await launchCamera({ mediaType: 'photo', quality: 1 }, response => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker')
+        return
+      }
+      if (response.errorCode) {
+        console.log('ImagePicker Error: ', response.errorMessage)
+        return
+      }
+      if (response.assets) {
+        setPhoto(response.assets[0].uri as string)
+      }
+    })
+  }
+
+  async function openGallery() {
+    await launchImageLibrary(
+      {
+        mediaType: 'photo',
+        quality: 1,
+        selectionLimit: 1,
+      },
+      response => {
+        if (response.didCancel) {
+          console.log('User cancelled image picker')
+          return
+        }
+        if (response.errorMessage) {
+          console.log('ImagePicker Error: ', response.errorMessage)
+          return
+        }
+        if (response.errorCode) {
+          console.log('ImagePicker Error: ', response.errorMessage)
+          return
+        }
+
+        if (response.assets) {
+          console.log('Tem foto', response.assets[0].uri)
+          setPhoto(response.assets[0].uri as string)
+        }
+      },
+    )
+  }
+
   return (
     <Screen>
       <HeaderScreens screenName="bottom_tap_balance" />
+      <ButtonRow
+        buttonPrimaryOnPress={openCamera}
+        buttonPrimaryTitle="Abrir Camera"
+        buttonSecondaryOnPress={openGallery}
+        buttonSecondaryTitle="Abrir Galeria"
+      />
+
+      {photo && (
+        <Image
+          source={{ uri: photo }}
+          style={{ width: '100%', height: 300, marginTop: 20 }}
+        />
+      )}
     </Screen>
   )
 }
--- src/screens/app/bottomScreens/nutrition/nutritionRecipe/NutritionRecipeScreen.tsx
+++ src/screens/app/bottomScreens/nutrition/nutritionRecipe/NutritionRecipeScreen.tsx
@@ -1,19 +1,77 @@
 import React from 'react'
-import { Header, Screen } from '@components'
+import { Button, Header, Screen } from '@components'
 import { AppRoutesProps } from 'src/routes/navigationType'
 
 export function NutritionRecipeScreen({
   route,
 }: AppRoutesProps<'NutritionRecipeScreen'>) {
   const { title } = route.params
+  const KEY_GPT = 'Aqui vai key do GPT-3'
+  // async function fetchRecipe() {
+  //   await fetch('https://api.openai.com/v1/chat/completions', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Authorization:
+  //         'Bearer',
+  //     },
+  //     body: JSON.stringify({
+  //       model: 'gpt-3.5-turbo',
+  //       messages: [
+  //         {
+  //           role: 'user',
+  //           content: `Você é um chefe de cozinha. Me indique uma receita utilizando o ingrediente ${title}.O retorno deve ser realizado em JSON, com os ingredientes no atributo "ingredients" em formato array e as etapas no atributo "steps" em formato array.me informe em forma de array a quantidade de "proteins", "carbohydrates", "fats" e "totalCalories" `,
+  //         },
+  //       ],
+  //       temperature: 0.2,
+  //       max_tokens: 100,
+  //       top_p: 1,
+  //     }),
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log(data.choices[0].message.content)
+  //     })
+  //     .catch(error => {
+  //       console.error('Error:', error)
+  //     })
+  // }
+
+  async function handleGenerate() {
+    const prompt = `Você é um chefe de cozinha. Me indique uma receita utilizando o ingrediente frango frito.O retorno deve ser realizado em JSON, com os ingredientes no atributo "ingredients" em formato array e as etapas no atributo "steps" em formato array.me informe em forma de array a quantidade de "proteins", "carbohydrates", "fats" e "totalCalories" `
+
+    fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${KEY_GPT}`,
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo-0125',
+        messages: [
+          {
+            role: 'user',
+            content: prompt,
+          },
+        ],
+        temperature: 0.2,
+        max_tokens: 500,
+        top_p: 1,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.choices[0].message.content)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 
   return (
     <Screen>
       <Header title={title} />
-      {/* Inserir dados via ia ver como fazer kkkk */}
-      {/* Todo inserir aqui uma foto */}
-      {/* Lista de ingredientes  */}
-      {/* Lista de modo de preparo */}
+      <Button onPress={handleGenerate} title="Teste" />
     </Screen>
   )
 }
